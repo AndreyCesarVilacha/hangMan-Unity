85336f1e63ab4425cbb2851a3a3ff55abea4a2d8
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2021-08-28 05:53:12 -0300

    taking a random word and passing to wordToFind

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public Text timeField;
+    public Text wordToFindField;
     private float time;
     private string[] wordsLocal = { "MATT", "JOANNE", "ROBERT", "MARRY JANE" };
+    private string choseWord;
+    private string hiddenWord;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Taking a random word from 'wordsLocal'
+        choseWord = wordsLocal[Random.Range(0, wordsLocal.Length)];
+        //Passing the rando word to 'wordToFindField'
+        wordToFindField.text = choseWord;
     }
 
     // Update is called once per frame
